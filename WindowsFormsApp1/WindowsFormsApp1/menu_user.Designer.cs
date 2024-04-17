52e7f2f756ef0f2fda220bb77ae16a7cebab0b75
Bebesova <127486007+Bebesova@users.noreply.github.com>
2024-04-17 20:30:17 +0500

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/menu_user.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class menu_user
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloUser = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DGRev = new System.Windows.Forms.DataGridView();
+            this.DGTitle = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGRev)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGTitle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // HelloUser
+            // 
+            this.HelloUser.AutoSize = true;
+            this.HelloUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.HelloUser.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.HelloUser.Location = new System.Drawing.Point(513, 23);
+            this.HelloUser.Name = "HelloUser";
+            this.HelloUser.Size = new System.Drawing.Size(160, 20);
+            this.HelloUser.TabIndex = 17;
+            this.HelloUser.Text = "С возвращением, ";
+            this.HelloUser.Click += new System.EventHandler(this.HelloUser_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 443);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 202);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "отзыв";
+            this.groupBox1.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 31);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 32);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(6, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Cохранить отзыв";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.richTextBox1.Location = new System.Drawing.Point(6, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(354, 68);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(64, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "- Оценка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(408, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 26);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Отзывы";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 26);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Произведения";
+            // 
+            // DGRev
+            // 
+            this.DGRev.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DGRev.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGRev.Location = new System.Drawing.Point(413, 97);
+            this.DGRev.Name = "DGRev";
+            this.DGRev.Size = new System.Drawing.Size(360, 173);
+            this.DGRev.TabIndex = 13;
+            // 
+            // DGTitle
+            // 
+            this.DGTitle.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DGTitle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGTitle.Location = new System.Drawing.Point(12, 97);
+            this.DGTitle.Name = "DGTitle";
+            this.DGTitle.Size = new System.Drawing.Size(370, 330);
+            this.DGTitle.TabIndex = 12;
+            // 
+            // menu_user
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(838, 665);
+            this.Controls.Add(this.HelloUser);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DGRev);
+            this.Controls.Add(this.DGTitle);
+            this.Name = "menu_user";
+            this.Text = "menu_user";
+            this.Load += new System.EventHandler(this.menu_user_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGRev)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGTitle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloUser;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DGRev;
+        private System.Windows.Forms.DataGridView DGTitle;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/menu_user.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class menu_user : Form
+    {
+        public menu_user()
+        {
+            InitializeComponent();
+        }
+        static string ConnString = @"Data Source=MEPHI3-MSSQL;Initial Catalog=BebeReviews;Integrated Security=True";
+       
+        SqlConnection sqlConnect = new SqlConnection(ConnString);
+
+        private void menu_user_Load(object sender, EventArgs e)
+        {
+            sqlConnect.Open();
+            SqlCommand Title = new SqlCommand();
+            Title.CommandText = $"SELECT * from works ";
+            Title.Connection = sqlConnect;
+            SqlDataAdapter adapter = new SqlDataAdapter(Title);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            DGTitle.DataSource = dataSet.Tables[0];
+            DGTitle.Columns[0].Visible = false;
+            sqlConnect.Close();
+            sqlConnect.Open();
+            SqlCommand Rating = new SqlCommand();
+            Rating.CommandText = $"SELECT * from ViewReview ";
+            Rating.Connection = sqlConnect;
+            SqlDataAdapter vwv = new SqlDataAdapter(Rating);
+            DataSet dvwv = new DataSet();
+            vwv.Fill(dvwv);
+            DGRev.DataSource = dvwv.Tables[0];
+            DGRev.Columns[0].Visible = false;
+            sqlConnect.Close();
+        }
+
+        private void HelloUser_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
